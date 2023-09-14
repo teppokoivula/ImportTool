f9fbeec642fc1e374d35219f3cf42f7b4ee6499c
Teppo Koivula <teppo.koivula@gmail.com>
2023-09-15 00:29:39 +0300

    Use pointer-events instead of disabled

--- ProcessImportTool.js
+++ ProcessImportTool.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	const processImportToolForm = document.getElementById('process-import-tool-form');
 	const processImportToolSubmit = document.getElementById('process-import-tool-submit');
 	processImportToolForm.addEventListener('submit', () => {
-		processImportToolSubmit.disabled = true;
+		processImportToolSubmit.setAttribute('style', 'pointer-events: none');
 		processImportToolSubmit.classList.add('ui-state-disabled');
 		const processImportToolSubmitIcon = document.createElement('i');
 		processImportToolSubmitIcon.setAttribute('class', 'uk-margin-small-left fa fa-circle-o-notch fa-spin');
